fabe72f999a1f4651c0f2ff7d815f4d5e30cfb4e
Dharmendra Singh Chaudhary <dharmendra.078bct016@tcioe.edu.np>
2023-11-02 21:38:24 +0545

    adding hasing password code

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,9 +21,20 @@ exports.userSignup = async ( req , res) => {
 
 exports.userLogin = async ( req , res) => {
     try{
+        const {email , password} = req.body ; 
+
+        const data = await userModel.checkPass(email , password);
+
+        console.log("dta",data);
+        if(!data) return res.redirect("/login");
+        
+        res.redirect("/");
 
     }
     catch(err) {
-        
+        console.log(err) ; 
+        res.status(500).json({
+            message:"internal error from login end point",
+        })
     }
 }
--- models/userModel.js
+++ models/userModel.js
@@ -38,6 +38,7 @@ userSchema.pre("save" , function (next) {
     if( !user.isModified("password")) return;
 
     const salt = randomBytes(16).toString() ;
+    // const salt= "helloworld";
 
     const hashedPassword = createHmac('sha256', salt)
                .update(user.password)
@@ -46,5 +47,23 @@ userSchema.pre("save" , function (next) {
     this.password = hashedPassword;
     next();
 } )
+
+
+userSchema.static("checkPass" , async function(email , password) {
+
+    const user = await this.findOne({email});
+    // console.log("user from checkpass" , user);
+    if(!user) throw new Error("no user found");
+
+    const salt = user.salt;
+    const proviedhashedPassword = createHmac('sha256', salt)
+    .update(password)
+    .digest('hex');
+
+    if( proviedhashedPassword !== user.password ) throw new Error("no password matched");
+
+    return user;
+})
+
 
 module.exports = model("user" , userSchema) ; 
